bfee6ec7ceb0853c182892b06059b73c6289ac98
shadab <shadab14meb346@gmail.com>
2020-12-16 00:06:32 +0530

    consoling the main conatiner having tweets actions

--- contentCopy.js
+++ contentCopy.js
@@ -4,12 +4,56 @@ var callback = function (mutationsList) {
 		(record) => record.addedNodes.length & addedNodes.push(...record.addedNodes)
 	);
 	const sections = addedNodes.filter((node) => node.nodeName === "SECTION");
-	// const sectionContainingTweets = [];
+	const _callback = function (_mutations) {
+		const addedNodes = [];
+		_mutations.forEach(
+			(record) =>
+				record.addedNodes.length & addedNodes.push(...record.addedNodes)
+		);
+		const addedDivNode = addedNodes.filter((node) => node.nodeName === "DIV");
+		const divHavingTweets = addedDivNode.filter((node) =>
+			node.innerText.includes(
+				`<div style="position: absolute; width: 100%; transform: translateY`
+			)
+		);
+		if (addedDivNode.length) {
+			const mainDivHavingTweetActions = [];
+			for (const div of addedDivNode) {
+				console.log(
+					div.querySelector(
+						".css-1dbjc4n.r-18u37iz.r-1wtj0ep.r-156q2ks.r-1mdbhws"
+					)
+				);
+			}
+		}
+		// addedDivNode.length &&
+		// 	console.log(
+		// 		addedDivNode.querySelector(
+		// 			".css-1dbjc4n.r-18u37iz.r-1wtj0ep.r-156q2ks.r-1mdbhws"
+		// 		)
+		// 	);
+		// if (
+		// 	addedNodes.innerText.includes(
+		// 		`<div style="position: absolute; width: 100%; transform: translateY`
+		// 	)
+		// ) {
+		// 	console.log(addedNodes);
+		// }
+		// const articles = addedNodes.filter((node) => node.nodeName);
+		// console.log(articles, "articles");
+	};
+
 	for (const section of sections) {
 		if (section.textContent.includes("Your Home Timeline")) {
-			console.log(section.lastChild.children[0].children);
-
-			// console.log(section.lastChild.children);
+			const targetSection = section;
+			const observer = new MutationObserver(_callback);
+			const targetNode = targetSection;
+			observer.observe(targetNode, {
+				childList: true,
+				attributes: true,
+				characterData: true,
+				subtree: true,
+			});
 		}
 	}
 };
