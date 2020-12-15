cd242e1215f526c3350708ec73af00268f1400d8
shadab <shadab14meb346@gmail.com>
2020-12-15 22:27:23 +0530

    console nodes

--- contentCopy.js
+++ contentCopy.js
@@ -3,20 +3,14 @@ var callback = function (mutationsList) {
 	mutationsList.forEach(
 		(record) => record.addedNodes.length & addedNodes.push(...record.addedNodes)
 	);
-	// const sections = addedNodes.filter((node) => node.nodeName === "SECTION");
+	const sections = addedNodes.filter((node) => node.nodeName === "SECTION");
 	// const sectionContainingTweets = [];
-	// for (const section of sections) {
-	// 	if (section.textContent.includes("Your Home Timeline")) {
-	// 		// console.log(section.lastChild.lastChild);
-	// 	}
-	// }
-	// console.log(addedNodes);
-	const textnode = document.createTextNode("BM");
+	for (const section of sections) {
+		if (section.textContent.includes("Your Home Timeline")) {
+			console.log(section.lastChild.children[0].children);
 
-	const allDiv = document.querySelectorAll(".r-1mdbhws");
-	for (const div of allDiv) {
-		div.style["background"] = "red";
-		// div.appendChild(textnode);
+			// console.log(section.lastChild.children);
+		}
 	}
 };
 
