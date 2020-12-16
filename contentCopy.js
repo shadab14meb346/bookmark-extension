1c0baeea0077d0906cd7eb506bf6b2c13ea2d91b
shadab <shadab14meb346@gmail.com>
2020-12-16 12:27:44 +0530

    code cleanup

--- contentCopy.js
+++ contentCopy.js
@@ -1,43 +1,37 @@
-var callback = function (mutationsList) {
+const callbackForSectionMutation = function (_mutations) {
+	const addedNodes = [];
+	_mutations.forEach(
+		(record) => record.addedNodes.length & addedNodes.push(...record.addedNodes)
+	);
+	const addedDivNodes = addedNodes.filter((node) => node.nodeName === "DIV");
+	if (addedDivNodes.length) {
+		for (const div of addedDivNodes) {
+			const mainDivHavingTweetActions = div.querySelector(
+				".css-1dbjc4n.r-18u37iz.r-1wtj0ep.r-156q2ks.r-1mdbhws"
+			);
+			if (mainDivHavingTweetActions) {
+				const newDiv = document.createElement("div");
+				newDiv.innerHTML = "BM";
+				newDiv.style.color = "#5b7082";
+				newDiv.style.display = "flex";
+				newDiv.style.alignItems = "center";
+				mainDivHavingTweetActions.appendChild(newDiv);
+			}
+		}
+	}
+};
+
+const callback = function (mutationsList) {
 	const addedNodes = [];
 	mutationsList.forEach(
 		(record) => record.addedNodes.length & addedNodes.push(...record.addedNodes)
 	);
 	const sections = addedNodes.filter((node) => node.nodeName === "SECTION");
-	const _callback = function (_mutations) {
-		const addedNodes = [];
-		_mutations.forEach(
-			(record) =>
-				record.addedNodes.length & addedNodes.push(...record.addedNodes)
-		);
-		const addedDivNode = addedNodes.filter((node) => node.nodeName === "DIV");
-		const divHavingTweets = addedDivNode.filter((node) =>
-			node.innerText.includes(
-				`<div style="position: absolute; width: 100%; transform: translateY`
-			)
-		);
-		if (addedDivNode.length) {
-			for (const div of addedDivNode) {
-				const mainDivHavingTweetActions = div.querySelector(
-					".css-1dbjc4n.r-18u37iz.r-1wtj0ep.r-156q2ks.r-1mdbhws"
-				);
-				if (mainDivHavingTweetActions) {
-					const newDiv = document.createElement("div");
-					newDiv.innerHTML = "BM";
-					newDiv.style.color = "#5b7082";
-					newDiv.style.display = "flex";
-					newDiv.style.alignItems = "center";
-					mainDivHavingTweetActions.appendChild(newDiv);
-				}
-			}
-		}
-	};
 
 	for (const section of sections) {
 		if (section.textContent.includes("Your Home Timeline")) {
-			const targetSection = section;
-			const observer = new MutationObserver(_callback);
-			const targetNode = targetSection;
+			const observer = new MutationObserver(callbackForSectionMutation);
+			const targetNode = section;
 			observer.observe(targetNode, {
 				childList: true,
 				attributes: true,
