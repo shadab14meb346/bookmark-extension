3d2ea20a462abfc46f8addd4ed768adf4647a9c6
shadab <shadab14meb346@gmail.com>
2020-12-20 12:26:20 +0530

    remove unrequired files

--- content1.js
+++
@@ -1,8 +0,0 @@
-console.log("content1 file running");
-
-window.addEventListener("click", wordSelected);
-// function wordSelected(e) {
-// 	console.log("word selected");
-// 	let selectedText = window.(e);
-// 	console.log(selectedText);
-// }
