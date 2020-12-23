89475f4e8f2ca23220a83b2081ec4d679caaf10e
shadab <shadab14meb346@gmail.com>
2020-12-23 12:17:33 +0530

    remove commented code

--- content.js
+++ content.js
@@ -37,19 +37,6 @@ function handleBMClick(bmButtonId, tweet, toggleDivId, x, y) {
 	chrome.runtime.sendMessage(message);
 }
 function addBMButton(mainDivHavingTweetActions, tweetUrl, tweetText, date) {
-	// const tweetUrl = mainDivHavingTweetActions.parentNode.parentNode.firstChild.lastChild.lastChild.firstChild.querySelectorAll(
-	// 	"a"
-	// )[1];
-	// const tweetText =
-	// 	mainDivHavingTweetActions.parentNode.firstChild.lastChild.lastChild
-	// 		.innerText;
-
-	// const date =
-	// 	mainDivHavingTweetActions.parentNode.parentNode.firstChild.firstChild
-	// 		.firstChild.firstChild.lastChild.lastChild.dateTime;
-	// const tweetMediaUrl =
-	// 	mainDivHavingTweetActions.parentNode.firstChild.lastChild.lastChild
-	// 		.innerText;
 	const containerId = uuidv4();
 	const newContainer = document.createElement("div");
 	newContainer.style.display = "flex";
@@ -104,54 +91,6 @@ function addBMButton(mainDivHavingTweetActions, tweetUrl, tweetText, date) {
 	}
 	insertAfter(newContainer, mainDivHavingTweetActions.childNodes[2]);
 }
-// const callbackForSectionMutation = function (_mutations) {
-// 	const addedNodes = [];
-// 	_mutations.forEach(
-// 		(record) => record.addedNodes.length & addedNodes.push(...record.addedNodes)
-// 	);
-// 	const addedDivNodes = addedNodes.filter((node) => node.nodeName === "DIV");
-// 	if (addedDivNodes.length) {
-// 		for (const div of addedDivNodes) {
-// 			const mainDivHavingTweetActions = div.querySelector(
-// 				".css-1dbjc4n.r-18u37iz.r-1wtj0ep.r-156q2ks.r-1mdbhws"
-// 			);
-// 			if (mainDivHavingTweetActions) {
-// 				addBMButton(mainDivHavingTweetActions);
-// 			}
-// 		}
-// 	}
-// };
-
-// const callback = function (mutationsList) {
-// 	const addedNodes = [];
-// 	mutationsList.forEach(
-// 		(record) => record.addedNodes.length & addedNodes.push(...record.addedNodes)
-// 	);
-// 	const sections = addedNodes.filter((node) => node.nodeName === "SECTION");
-
-// 	for (const section of sections) {
-// 		if (section.textContent.includes("Your Home Timeline")) {
-// 			const observer = new MutationObserver(callbackForSectionMutation);
-// 			const targetNode = section;
-// 			observer.observe(targetNode, {
-// 				childList: true,
-// 				attributes: true,
-// 				characterData: true,
-// 				subtree: true,
-// 			});
-// 		}
-// 	}
-// };
-
-// // Create an observer instance linked to the callback function
-// const observer = new MutationObserver(callback);
-// const targetNode = document.querySelector("#react-root");
-// observer.observe(targetNode, {
-// 	childList: true,
-// 	attributes: true,
-// 	characterData: true,
-// 	subtree: true,
-// });
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 	if (request.text == "saved successfully") {
@@ -276,10 +215,6 @@ ready("article", (article) => {
 	if (timeTag) {
 		date = timeTag.dateTime;
 	}
-	/**some tweets like this 'https://twitter.com/MicrosoftIndia/status/1341209990359638017'
-	doesn't have time on top but below so for them to get time will need to look into other div
-	 */
-	//there are two cases. 1. Tweet url is available 2. Tweet Url and date both not available(this is when user clicks on the tweet having replies and then trying to save it)
 	//there are edge cases when user have add blocker it can give some bugs
 	if (!date) {
 		const allAnchorTagsInArticle = [
